90156d1225367f3b94682563d2c1a51506580d2b
handemelindo <handemelindo@localhost.localdomain>
2015-01-18 05:37:50 -0500

    remove all the mass

--- .emacs.d/elpa/molokai-theme-20130828.0/molokai-theme-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; molokai-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("molokai-theme.el") (21686 35824 721000
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; molokai-theme-autoloads.el ends here
